597df7aed10708d5506ee3c85c9c3633413cff99
Anand Ayyasamy <c-anand.ayyasamy@charter.com>
2019-01-01 23:07:33 -0600

    db api update

---
+++ src/main/scala/com/org/data/detect/malfunctions/Utils/DMFConfig.scala
@@ -0,0 +1,34 @@
+package com.org.data.detect.malfunctions.Utils
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+
+object DMFConfig {
+
+
+  def orientWrOpts(props: mutable.Map[String, String]): Map[String, String] = {
+    val params = Map[String, String](
+      "dburl" ->"db",
+      "user" -> "user",
+      "password" -> "password",
+      "class" -> "test_table"
+
+    )
+    params
+
+  }
+
+
+  def orientRdOpts(props: mutable.Map[String, String]): Map[String, String] = {
+    val params = Map[String, String](
+      "dburl" -> "db",
+      "user" -> "user",
+      "password" -> "password",
+      "class" -> "test_table",
+      "query" -> s"select * from $test_table where teststring = 'asdf'"
+    )
+    params
+
+  }
+
+}
---
+++ src/main/scala/com/org/data/detect/malfunctions/constants/App_constants.scala
@@ -0,0 +1,8 @@
+package com.org.data.detect.malfunctions.constants
+
+/**
+  * This class is used to define all the application constants and values.
+  */
+object App_constants {
+
+}
